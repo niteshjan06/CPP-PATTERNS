33a0a0a82091838cbcfc0bdfed653094804b3a0c
niteshjan06 <niteshjan06@gmail.com>
2026-07-03 18:56:31 +0530

    Add C++ program to print asterisks in descending order
    pattern using  loops

---
+++ 3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    
+    cout << "Enter your input: ";
+    cin >> n;
+
+    for(int i=n;i>=1;i--){
+        for(int j=i;j>=1;j--){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
